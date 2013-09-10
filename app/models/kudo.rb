233bf7227db6445613a208ca012d5979fe984a1a
Stephanie <stephaine@thepotionsmaster.net>
2013-09-10 13:51:26 -0500

    Changes in work model

--- app/models/kudo.rb
+++ app/models/kudo.rb
@@ -1,17 +1,17 @@
 class Kudo < ActiveRecord::Base
-  belongs_to :pseud
+  belongs_to :pseud         
   belongs_to :commentable, :polymorphic => true
 
-  validates_uniqueness_of :pseud_id,
-    :scope => [:commentable_id, :commentable_type],
-    :message => ts("^You have already left kudos here. :)"),
+  validates_uniqueness_of :pseud_id, 
+    :scope => [:commentable_id, :commentable_type], 
+    :message => ts("^You have already left kudos here. :)"), 
     :if => "!pseud.nil?"
 
-  validates_uniqueness_of :ip_address,
-    :scope => [:commentable_id, :commentable_type],
-    :message => ts("^You have already left kudos here. :)"),
+  validates_uniqueness_of :ip_address, 
+    :scope => [:commentable_id, :commentable_type], 
+    :message => ts("^You have already left kudos here. :)"), 
     :if => "!ip_address.blank?"
-
+  
   scope :with_pseud, where("pseud_id IS NOT NULL")
   scope :by_guest, where("pseud_id IS NULL")
 
@@ -23,8 +23,5 @@ class Kudo < ActiveRecord::Base
       "guest"
     end
   end
-
-  def dup?
-    errors.values.to_s.match /already left kudos/
-  end
+  
 end
