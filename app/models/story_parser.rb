7d416b46eee8d86759c4cd8805139a085046c5ba
Stephanie <stephaine@thepotionsmaster.net>
2014-02-01 16:11:08 -0600

    forgot caps on an item

--- app/models/story_parser.rb
+++ app/models/story_parser.rb
@@ -441,7 +441,7 @@ class StoryParser
   def set_work_attributes_from_mash(work,mash,options={})
     raise Error, "Work could not be... well something is broke!" if work.nil?
     binding.pry
-    work.imported_from_url = mash.importwork.source_url
+    work.imported_from_url = mash.importwork.work.source_url
     if mash.importwork.chapter.class.to_s ="Array"
       work.expected_number_of_chapters = mash.importwork.work.chapter.length
     else
