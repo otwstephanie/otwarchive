dc5f30bf82a49d3b741f643b7df38cb1c0db4a21
Stephanie <stephaine@thepotionsmaster.net>
2014-02-01 16:01:32 -0600

    forgot caps on an item

--- app/models/story_parser.rb
+++ app/models/story_parser.rb
@@ -278,7 +278,7 @@ class StoryParser
 
   # Parses the text of a story, optionally from a given location.
   def parse_story(story, location, options = {})
-    m = location
+    m = story
     work_params = parse_common(story, location, options)
 
       binding.pry
