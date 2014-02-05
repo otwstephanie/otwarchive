eae28d5749b3a10a8a207103c52e92a8f8aa1c68
Stephanie <stephaine@thepotionsmaster.net>
2014-02-05 16:10:46 -0600

    fixed things from od comments

--- app/models/story_parser.rb
+++ app/models/story_parser.rb
@@ -74,6 +74,7 @@ class StoryParser
   failed_urls = []
   errors = []
   url = "nothing"
+  binding.pry
   mashed_works.importworks.each do |iw|
     begin
       work_mash = Hashie::Mash.new(Hash[*iw.flatten])
