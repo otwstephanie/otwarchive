487aef704c9bd231a3451bd3cbc8c45ae85f8358
Stephanie <stephaine@thepotionsmaster.net>
2014-02-08 16:27:28 -0600

    fixed things from od comments

--- app/models/story_parser.rb
+++ app/models/story_parser.rb
@@ -391,7 +391,7 @@ class StoryParser
   #return options hash with authors added from xml
   def xml_hash_to_mash_assign_authors(mash)
     options = {}
-
+     binding.pry
     if mash.author.class.to_s == "Array"
     options[:author_name] = m.author[0].name
     options[:author_email] = m.author[0].email
